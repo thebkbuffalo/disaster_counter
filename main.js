a0f08d546f255ff0a7ba0c4eb133926e64df3fdf
Evan Berg <evan.m.berg@gmail.com>
2016-03-15 14:21:32 -0700

    gotta plan and some notes

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 
   var clock = new FlipClock($('.your-clock'), {
-    // ... your options here
   });
+
+
+  // have this save the current time to localStorage somehow, and than make the clock start from that point everytime its initialized...this should work, also add a reset button
+  $('.save').on('click', function(){
+    console.log(clock.timer.count);
+    localStorage.setItem('time', clock.time.count);
+  });
+  // console.log(clock.timer.count);
 });
