be3a251313c7d8812e9e297d1f5f492064063e1d
Evan Berg <evan.m.berg@gmail.com>
2016-03-17 12:49:12 -0700

    i think i fixed the issue!

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ var clock;
 var time;
 $(document).ready(function(){
 
-  // time = localStorage.setItem('time', 0);
   function save() {
     localStorage.setItem('time', clock.timer.count);
     console.log(localStorage.getItem('time'));
+
   }
 
   clock = new FlipClock($('.your-clock'), localStorage.getItem('time'), {
 
   });
+  clock.timer.count = localStorage.getItem('time');
 
 
   setInterval(save, 2000);
